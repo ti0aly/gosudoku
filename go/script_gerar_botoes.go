d72a920916755fa6ff6932fbd60a76d91006f027
aly <alyreinert@gmail.com>
2024-08-17 21:16:12 -0300

    sabado em casa

---
+++ go/script_gerar_botoes.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+	"strconv"
+)
+
+func main() {
+	caminhoArquivo := "gerahtmlbotoes.txt"
+	file, _ := os.OpenFile(caminhoArquivo, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	defer file.Close()
+
+	for i:=1;i<=81;i++{
+		file.WriteString("<div class=\"grid-item\"><input type=\"text\" maxlength=\"1\" value=\""+strconv.Itoa(i)+"\"></div>\n")
+	}
+
+
+}
