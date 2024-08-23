c5db8f0882b0038d02b52cc87e985594d1331abd
aly <alyreinert@gmail.com>
2024-08-23 18:51:30 -0300

    testando git pages

--- geradorTabuleiros/main.go
+++ geradorTabuleiros/main.go
@@ -1,6 +1,11 @@
 package main
 
-// Esse mapa garante o posicionamento no quadrante
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 var quadrante = [][]int {
 	0: {0, 0, 0, 1, 1, 1, 2, 2, 2},
 	1: {0, 0, 0, 1, 1, 1, 2, 2, 2},
@@ -13,7 +18,6 @@ var quadrante = [][]int {
 	8: {6, 6, 6, 7, 7, 7, 8, 8, 8},
 	}
 
-// Esse mapa gera a variável vazia inicial
 var tabuleiro = [][]int{
 	0: {0, 0, 0, 0, 0, 0, 0, 0, 0},
 	1: {0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -26,7 +30,6 @@ var tabuleiro = [][]int{
 	8: {0, 0, 0, 0, 0, 0, 0, 0, 0},
 		} 
 
-// determina todas as posições de um quadrante
 var quadrantes = [][]int{
 	0: {tabuleiro[0][0], tabuleiro[0][1], tabuleiro[0][2], tabuleiro[1][0], tabuleiro[1][1], tabuleiro[1][2], tabuleiro[2][0], tabuleiro[2][1], tabuleiro[2][2]},
 	1: {tabuleiro[0][3], tabuleiro[0][4], tabuleiro[0][5], tabuleiro[1][3], tabuleiro[1][4], tabuleiro[1][5], tabuleiro[2][3], tabuleiro[2][4], tabuleiro[2][5]},
@@ -39,48 +42,154 @@ var quadrantes = [][]int{
 	8: {tabuleiro[6][6], tabuleiro[6][7], tabuleiro[6][8], tabuleiro[7][6], tabuleiro[7][7], tabuleiro[7][8], tabuleiro[8][6], tabuleiro[8][7], tabuleiro[8][8]},
 		}   
 		
-func main() {
-	go imprimeDemoraEmSegundos()
-	for i := 0; i < 50; i++ {
-		tabuleiroValido := geraUmTabuleiroValido(tabuleiro)
-		imprimeTabuleiro(tabuleiroValido)
-		guardaVetor(tabuleiroValido)
-	}
-	//fmt.Println("\nA JANELA FECHARÁ EM 20 SEGUNDOS")
-	//time.Sleep(20 * time.Second)
+func geraUmTabuleiroValido(tabuleiro [][]int) [][]int {
+	var posicoesPossiveis [][]int
+	var linha, coluna int 
+	count := 0
+	for { 
+		
+		count++
+		for num := 1; num <= 9; num++ {
+			for i := 0; i < 9; i++ {
+				posicoesPossiveis = verificaTodasAsPosicoesPossiveisDeUmNumero(num, tabuleiro)
+				if len(posicoesPossiveis) > 0 {
+					linha, coluna = sorteiaNumeroDeUmVetor(posicoesPossiveis)
+					tabuleiro[linha][coluna] = num
+				}
+			}
+		}
 
+	if !verificaTabuleiro(tabuleiro) {
+		fmt.Println("<- esta foi a demora em segundos\n\nNumero de tentativas: ", count)
+
+		break
+
+	} else {
+		tabuleiro = [][]int{
+			0: {0, 0, 0, 0, 0, 0, 0, 0, 0},
+			1: {0, 0, 0, 0, 0, 0, 0, 0, 0},
+			2: {0, 0, 0, 0, 0, 0, 0, 0, 0},
+			3: {0, 0, 0, 0, 0, 0, 0, 0, 0},
+			4: {0, 0, 0, 0, 0, 0, 0, 0, 0},
+			5: {0, 0, 0, 0, 0, 0, 0, 0, 0},
+			6: {0, 0, 0, 0, 0, 0, 0, 0, 0},
+			7: {0, 0, 0, 0, 0, 0, 0, 0, 0},
+			8: {0, 0, 0, 0, 0, 0, 0, 0, 0},
+				} 
+	}
+
+
+	}
+	return tabuleiro
+
+}
+
+func sorteiaNumeroDeUmVetor(vetor [][]int) (int, int) {
+	rand.Seed(time.Now().UnixNano())
+	sorteado := vetor[rand.Intn(len(vetor))]
+	x, y := sorteado[0], sorteado[1]
+	return x, y
+}
+
+func verificaTodasAsPosicoesPossiveisDeUmNumero(numero int, tabuleiro [][]int) [][]int {
+	var posicoesPossiveis [][]int 
+	for linha := 0; linha < 9; linha++ {
+		for coluna := 0; coluna < 9; coluna++ {
+			if verificaNumeroPosicao(numero, linha, coluna, tabuleiro) {
+				posicoesPossiveis = append(posicoesPossiveis, []int{linha, coluna})
+			}
+		}
+	}
+	return posicoesPossiveis
+}
+
+// verifica se tem 0 no tabuleiro
+func verificaTabuleiro (tabuleiro [][]int) bool {
+	temZero := false
+	for c := 0; c < 9; c++ {
+		for i := 0; i < 9; i++ {
+			if tabuleiro [c][i] == 0 {
+				temZero = true
+			}
+		}
+	}
+	return temZero
+}
+
+
+func verificaNumeroPosicao(numero, linha, coluna int, tabuleiro [][]int) bool {
+	if !verifyColuna(numero, coluna, tabuleiro) && !verify(numero, tabuleiro[linha]) && !verificaQuadranteDeUmTabuleiro(numero, quadrante[linha][coluna], tabuleiro)  && tabuleiro[linha][coluna] == 0 {
+		return true
+	}
+	return false
+}
+
+func zeraNumeroIndicadoNoTabuleiro(numero int, tabuleiro [][]int) [][]int {
+	for linha := 0; linha < 9; linha ++ {
+		for coluna:= 0; coluna < 9; coluna++ {
+			if numero == tabuleiro[linha][coluna] {
+				tabuleiro[linha][coluna] = 0
+			}
+		}
+	}
+	return tabuleiro
+}
+
+func verificaQuadranteDeUmTabuleiro(numero, numeroDoQuadrante int, tabuleiro [][]int) bool {
+	defineQuadrantes := atribuiQuadrantes(tabuleiro)
+	return verify(numero, defineQuadrantes[numeroDoQuadrante]) 
 }
 
 
 
 
-/* 
-tabuleiro = [][]int{
-	0: {1, 0, 7, 0, 0, 6, 4, 5, 0},
-	1: {0, 2, 5, 3, 4, 0, 0, 0, 8},
-	2: {0, 6, 0, 0, 0, 1, 0, 7, 0},
-	3: {0, 5, 3, 0, 0, 0, 0, 2, 9},
-	4: {6, 1, 0, 0, 0, 9, 8, 0, 0},
-	5: {0, 0, 0, 6, 0, 2, 0, 0, 7},
-	6: {0, 0, 1, 0, 9, 3, 2, 0, 0},
-	7: {0, 0, 8, 0, 0, 0, 0, 0, 0},
-	8: {0, 4, 0, 0, 7, 8, 5, 9, 1},
+func verificaQuantosZerosTabuleiro (tabuleiro [][]int) int {
+	temZero := 0
+	for c := 0; c < 9; c++ {
+		for i := 0; i < 9; i++ {
+			if tabuleiro [c][i] == 0 {
+				temZero++
+			}
 		}
- */
-		
-/*  tabuleiro = [][]int{
-	0: {0, 0, 0, 0, 0, 0, 0, 0, 0},
-	1: {0, 0, 0, 0, 0, 0, 0, 0, 0},
-	2: {0, 0, 0, 0, 0, 0, 0, 0, 0},
-	3: {0, 0, 0, 0, 0, 0, 0, 0, 0},
-	4: {0, 0, 0, 0, 0, 0, 0, 0, 0},
-	5: {0, 0, 0, 0, 0, 0, 0, 0, 0},
-	6: {0, 0, 0, 0, 0, 0, 0, 0, 0},
-	7: {0, 0, 0, 0, 0, 0, 0, 0, 0},
-	8: {0, 0, 0, 0, 0, 0, 0, 0, 0},
-		} */
+	}
+	return temZero
+}
 
+func verify(x int, vetor []int) bool {
+	for _, v := range vetor {
+        if v == x {
+            return true
+        }
+    }
+    return false
+}
 
+func verifyColuna(x, coluna int, tabuleiro [][]int) bool {
+	for i := 0; i < 9; i++ {
+        if tabuleiro[i][coluna] == x {
+            return true
+        }
+    }
+    return false
+}
 
+func atribuiQuadrantes(tabuleiro [][]int) [][]int {
+	var quadrantes = [][]int{
+		0: {tabuleiro[0][0], tabuleiro[0][1], tabuleiro[0][2], tabuleiro[1][0], tabuleiro[1][1], tabuleiro[1][2], tabuleiro[2][0], tabuleiro[2][1], tabuleiro[2][2]},
+		1: {tabuleiro[0][3], tabuleiro[0][4], tabuleiro[0][5], tabuleiro[1][3], tabuleiro[1][4], tabuleiro[1][5], tabuleiro[2][3], tabuleiro[2][4], tabuleiro[2][5]},
+		2: {tabuleiro[0][6], tabuleiro[0][7], tabuleiro[0][8], tabuleiro[1][6], tabuleiro[1][7], tabuleiro[1][8], tabuleiro[2][6], tabuleiro[2][7], tabuleiro[2][8]},
+		3: {tabuleiro[3][0], tabuleiro[3][1], tabuleiro[3][2], tabuleiro[4][0], tabuleiro[4][1], tabuleiro[4][2], tabuleiro[5][0], tabuleiro[5][1], tabuleiro[5][2]},
+		4: {tabuleiro[3][3], tabuleiro[3][4], tabuleiro[3][5], tabuleiro[4][3], tabuleiro[4][4], tabuleiro[4][5], tabuleiro[5][3], tabuleiro[5][4], tabuleiro[5][5]},
+		5: {tabuleiro[3][6], tabuleiro[3][7], tabuleiro[3][8], tabuleiro[4][6], tabuleiro[4][7], tabuleiro[4][8], tabuleiro[5][6], tabuleiro[5][7], tabuleiro[5][8]},
+		6: {tabuleiro[6][0], tabuleiro[6][1], tabuleiro[6][2], tabuleiro[7][0], tabuleiro[7][1], tabuleiro[7][2], tabuleiro[8][0], tabuleiro[8][1], tabuleiro[8][2]},
+		7: {tabuleiro[6][3], tabuleiro[6][4], tabuleiro[6][5], tabuleiro[7][3], tabuleiro[7][4], tabuleiro[7][5], tabuleiro[8][3], tabuleiro[8][4], tabuleiro[8][5]},
+		8: {tabuleiro[6][6], tabuleiro[6][7], tabuleiro[6][8], tabuleiro[7][6], tabuleiro[7][7], tabuleiro[7][8], tabuleiro[8][6], tabuleiro[8][7], tabuleiro[8][8]},
+			}   
+	
+	return quadrantes
+}
 
-
+func main() {
+	tabuleiroValido := geraUmTabuleiroValido(tabuleiro)
+	fmt.Println(tabuleiroValido)
+}
--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-const tabuleiro = [[8, 6, 9, 2, 7, 5, 3, 4, 1],[2, 5, 4, 8, 3, 1, 6, 9, 7],
-[7, 1, 3, 6, 4, 9, 2, 8, 5],[5, 3, 1, 7, 8, 6, 9, 2, 4],[9, 7, 6, 4, 5, 2, 1, 3, 8],[4, 8, 2, 1, 9, 3, 7, 5, 6],[3, 9, 7, 5, 6, 8, 4, 1, 2],[1, 4, 8, 9, 2, 7, 5, 6, 3],[6, 2, 5, 3, 1, 4, 8, 7, 9]];
+let tabuleiro = Array(9).fill().map(() => Array(9).fill(0));
 
 preencheCampo(tabuleiro);
 
-document.getElementById('atualizaTabuleiro').addEventListener('click', () => {trocaTabuleiro()});
-
-function trocaTabuleiro() {
-    const novo = [[8, 3, 4, 2, 6, 5, 7, 1, 9], [6, 5, 1, 9, 7, 3, 8, 4, 2], [2, 7, 9, 8, 4, 1, 3, 5, 6], [1, 6, 3, 5, 2, 9, 4, 8, 7], [5, 8, 2, 4, 1, 7, 9, 6, 3], [9, 4, 7, 3, 8, 6, 5, 2, 1], [3, 1, 8, 7, 5, 2, 6, 9, 4], [7, 2, 5, 6, 9, 4, 1, 3, 8], [4, 9, 6, 1, 3, 8, 2, 7, 5]];
-    preencheCampo(novo);
-}
+document.getElementById('atualizaTabuleiro').addEventListener('click', () => {geraUmTabuleiroValido(tabuleiro);});
 
 function preencheCampo(tabuleiro) {
     let idCampo = 1;
@@ -23,5 +17,113 @@ function preencheCampo(tabuleiro) {
 
 function autor() {
     var div = document.getElementById("conteudoPagina");
-    div.innerHTML = "<h1>Alysson</h1> <p>Este projeto começou Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, repudiandae unde nemo aut ex beatae optio nisi nulla id quisquam ducimus dolore mollitia cum, eum consequatur modi obcaecati, cumque exercitationem?</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt maiores temporibus excepturi! Reiciendis molestias nisi nulla officia corrupti deleniti fuga quisquam. Velit dolores repudiandae aspernatur fuga? Fugit porro magni sed.</p>";
+    div.innerHTML = '<h1>Alysson</h1> <p>Este projeto começou Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, repudiandae unde nemo aut ex beatae optio nisi nulla id quisquam ducimus dolore mollitia cum, eum consequatur modi obcaecati, cumque exercitationem?</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt maiores temporibus excepturi! Reiciendis molestias nisi nulla officia corrupti deleniti fuga quisquam. Velit dolores repudiandae aspernatur fuga? Fugit porro magni sed.</p>';
 }
+
+function sorteiaNumeroDeUmVetor(vetor) {
+    const sorteado = vetor[Math.floor(Math.random() * vetor.length)];
+    const [x, y] = sorteado;
+    return [x, y];
+}
+
+function verificaTodasAsPosicoesPossiveisDeUmNumero(numero, tabuleiro) {
+    let posicoesPossiveis = [];
+    for (let linha = 0; linha < 9; linha++) {
+        for (let coluna = 0; coluna < 9; coluna++) {
+            if (verificaNumeroPosicao(numero, linha, coluna, tabuleiro)) {
+                posicoesPossiveis.push([linha, coluna]);
+            }
+        }
+    }
+    return posicoesPossiveis;
+}
+
+function verificaTabuleiro(tabuleiro) {
+    let temZero = false;
+    for (let c = 0; c < 9; c++) {
+        for (let i = 0; i < 9; i++) {
+            if (tabuleiro[c][i] === 0) {
+                temZero = true;
+            }
+        }
+    }
+    return temZero;
+}
+
+function verificaNumeroPosicao(numero, linha, coluna, tabuleiro) {
+    return !verifyColuna(numero, coluna, tabuleiro) && !verify(numero, tabuleiro[linha]) && !verificaQuadranteDeUmTabuleiro(numero, quadrante[linha][coluna], tabuleiro) && tabuleiro[linha][coluna] === 0;
+}
+
+function verificaQuadranteDeUmTabuleiro(numero, numeroDoQuadrante, tabuleiro) {
+    const defineQuadrantes = atribuiQuadrantes(tabuleiro);
+    return verify(numero, defineQuadrantes[numeroDoQuadrante]);
+}
+
+function atribuiQuadrantes(tabuleiro) {
+    return [
+        [tabuleiro[0][0], tabuleiro[0][1], tabuleiro[0][2], tabuleiro[1][0], tabuleiro[1][1], tabuleiro[1][2], tabuleiro[2][0], tabuleiro[2][1], tabuleiro[2][2]],
+        [tabuleiro[0][3], tabuleiro[0][4], tabuleiro[0][5], tabuleiro[1][3], tabuleiro[1][4], tabuleiro[1][5], tabuleiro[2][3], tabuleiro[2][4], tabuleiro[2][5]],
+        [tabuleiro[0][6], tabuleiro[0][7], tabuleiro[0][8], tabuleiro[1][6], tabuleiro[1][7], tabuleiro[1][8], tabuleiro[2][6], tabuleiro[2][7], tabuleiro[2][8]],
+        [tabuleiro[3][0], tabuleiro[3][1], tabuleiro[3][2], tabuleiro[4][0], tabuleiro[4][1], tabuleiro[4][2], tabuleiro[5][0], tabuleiro[5][1], tabuleiro[5][2]],
+        [tabuleiro[3][3], tabuleiro[3][4], tabuleiro[3][5], tabuleiro[4][3], tabuleiro[4][4], tabuleiro[4][5], tabuleiro[5][3], tabuleiro[5][4], tabuleiro[5][5]],
+        [tabuleiro[3][6], tabuleiro[3][7], tabuleiro[3][8], tabuleiro[4][6], tabuleiro[4][7], tabuleiro[4][8], tabuleiro[5][6], tabuleiro[5][7], tabuleiro[5][8]],
+        [tabuleiro[6][0], tabuleiro[6][1], tabuleiro[6][2], tabuleiro[7][0], tabuleiro[7][1], tabuleiro[7][2], tabuleiro[8][0], tabuleiro[8][1], tabuleiro[8][2]],
+        [tabuleiro[6][3], tabuleiro[6][4], tabuleiro[6][5], tabuleiro[7][3], tabuleiro[7][4], tabuleiro[7][5], tabuleiro[8][3], tabuleiro[8][4], tabuleiro[8][5]],
+        [tabuleiro[6][6], tabuleiro[6][7], tabuleiro[6][8], tabuleiro[7][6], tabuleiro[7][7], tabuleiro[7][8], tabuleiro[8][6], tabuleiro[8][7], tabuleiro[8][8]],
+    ];
+}
+
+function geraUmTabuleiroValido(tabuleiro) {
+    let posicoesPossiveis;
+    let linha, coluna;
+    let count = 0;
+
+
+
+    while (true) {
+        count++;
+        for (let num = 1; num <= 9; num++) {
+            for (let i = 0; i < 9; i++) {
+                posicoesPossiveis = verificaTodasAsPosicoesPossiveisDeUmNumero(num, tabuleiro);
+                if (posicoesPossiveis.length > 0) {
+                    [linha, coluna] = sorteiaNumeroDeUmVetor(posicoesPossiveis);
+                    tabuleiro[linha][coluna] = num;
+                    
+                }
+            }
+        }
+
+        if (!verificaTabuleiro(tabuleiro)) {
+            preencheCampo(tabuleiro);
+            console.log("Numero de tentativas: ", count);
+            break;
+        } else {
+            tabuleiro = Array(9).fill().map(() => Array(9).fill(0));
+            preencheCampo(tabuleiro);
+        }
+    }
+
+    console.log(tabuleiro);
+}
+
+function verify(x, vetor) {
+    return vetor.includes(x);
+}
+
+function verifyColuna(x, coluna, tabuleiro) {
+    return tabuleiro.some(row => row[coluna] === x);
+}
+
+// Exemplo de uso
+const quadrante = [
+    [0, 0, 0, 1, 1, 1, 2, 2, 2],
+    [0, 0, 0, 1, 1, 1, 2, 2, 2],
+    [0, 0, 0, 1, 1, 1, 2, 2, 2],
+    [3, 3, 3, 4, 4, 4, 5, 5, 5],
+    [3, 3, 3, 4, 4, 4, 5, 5, 5],
+    [3, 3, 3, 4, 4, 4, 5, 5, 5],
+    [6, 6, 6, 7, 7, 7, 8, 8, 8],
+    [6, 6, 6, 7, 7, 7, 8, 8, 8],
+    [6, 6, 6, 7, 7, 7, 8, 8, 8]
+];
+
