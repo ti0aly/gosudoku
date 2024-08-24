9ea438727c9b74bca0587c4627daa229c2cc3ab9
aly <alyreinert@gmail.com>
2024-08-24 20:26:20 -0300

    tentando fazer aproveitamento de main

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function autor() {
 
 function projeto() {
     console.log('chamou o projeto');
-    fetch('projeto.html')
+    fetch('https://ti0aly.github.io/gosudoku/projeto.html')
     .then(response => response.text())
     .then(data => {
       // Cria um DOMParser para extrair o conteúdo do <main>
@@ -30,7 +30,7 @@ function projeto() {
       const mainContent = doc.querySelector('main').innerHTML;
     
       // Insere o conteúdo no main atual
-      document.getElementById('main-content').innerHTML = mainContent;
+      document.getElementById('conteudoPagina').innerHTML = mainContent;
     })
     .catch(error => console.error('Erro ao carregar o conteúdo:', error));
 }
@@ -41,8 +41,19 @@ function ranking() {
 }
 
 function jogar() {
-    var div = document.getElementById("conteudoPagina");
-    div.innerHTML = '<h1>Jogue agora mesmo</h1> <p>PLAAAAAYY em ipsum dolor sit amet consectetur adipisicing elit. Quae, repudiandae unde nemo aut ex beatae optio nisi nulla id quisquam ducimus dolore mollitia cum, eum consequatur modi obcaecati, cumque exercitationem?</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt maiores temporibus excepturi! Reiciendis molestias nisi nulla officia corrupti deleniti fuga quisquam. Velit dolores repudiandae aspernatur fuga? Fugit porro magni sed.</p>';
+    console.log('chamou o jogo');
+    fetch('https://ti0aly.github.io/gosudoku/jogo.html')
+    .then(response => response.text())
+    .then(data => {
+      // Cria um DOMParser para extrair o conteúdo do <main>
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(data, 'text/html');
+      const mainContent = doc.querySelector('main').innerHTML;
+    
+      // Insere o conteúdo no main atual
+      document.getElementById('conteudoPagina').innerHTML = mainContent;
+    })
+    .catch(error => console.error('Erro ao carregar o conteúdo:', error));
 }
 
 function sorteiaNumeroDeUmVetor(vetor) {
