b160c8ca545871ac9e521570f47a3479f840b636
aly <alyreinert@gmail.com>
2024-08-27 13:31:16 -0300

    botoes funcionando, readonly aplicado, timer rodando no jogo

--- scriptjogo.js
+++ scriptjogo.js
@@ -1,3 +1,7 @@
+var erros = 0; 
+var tabuleiroAtual;
+var inputSelecionada = null;
+
 function loadCabecalhoeRodape() {
     fetch('https://ti0aly.github.io/gosudoku/header.html')
         .then(response => response.text())
@@ -9,136 +13,81 @@ function loadCabecalhoeRodape() {
 }
 
 window.onload = loadCabecalhoeRodape;
+document.getElementById('jogarNovamente').style.display = 'none';
+document.getElementById('registraRanking').style.display = 'none';
+document.getElementById('digitaNome').style.display = 'none';
 
-var erros = 0; 
+document.querySelectorAll('.inputMaroto').forEach(input => {
+    input.addEventListener('focus', function() {
+        inputSelecionada = this;
+        console.log(inputSelecionada.id);
+    });
+});
 
+document.getElementById('atualizaTabuleiroOculto1').addEventListener('click', () => {inicioDeJogo(1), document.getElementById('tituloPagina').innerHTML = 'Nível Moleza';});
+document.getElementById('atualizaTabuleiroOculto2').addEventListener('click', () => {inicioDeJogo(30), document.getElementById('tituloPagina').innerHTML = 'Nível Fácil';});
+document.getElementById('atualizaTabuleiroOculto3').addEventListener('click', () => {inicioDeJogo(40), document.getElementById('tituloPagina').innerHTML = 'Nível Médio';});
+document.getElementById('atualizaTabuleiroOculto4').addEventListener('click', () => {inicioDeJogo(50), document.getElementById('tituloPagina').innerHTML = 'Nível Difícil';});
+document.getElementById('atualizaTabuleiroOculto5').addEventListener('click', () => {inicioDeJogo(65), document.getElementById('tituloPagina').innerHTML = 'Nível Extremo';});
+document.getElementById('num1').addEventListener('click', () => {preencheInputSelecionada(1)});
+document.getElementById('num2').addEventListener('click', () => {preencheInputSelecionada(2)});
+document.getElementById('num3').addEventListener('click', () => {preencheInputSelecionada(3)});
+document.getElementById('num4').addEventListener('click', () => {preencheInputSelecionada(4)});
+document.getElementById('num5').addEventListener('click', () => {preencheInputSelecionada(5)});
+document.getElementById('num6').addEventListener('click', () => {preencheInputSelecionada(6)});
+document.getElementById('num7').addEventListener('click', () => {preencheInputSelecionada(7)});
+document.getElementById('num8').addEventListener('click', () => {preencheInputSelecionada(8)});
+document.getElementById('num9').addEventListener('click', () => {preencheInputSelecionada(9)});
+document.getElementById('registraRanking').addEventListener('click', () => {registraRanking()});
 
-
-
-
-document.getElementById('atualizaTabuleiroOculto1').addEventListener('click', () => {inicioDeJogo(1), document.getElementById('nivelDoJogo').innerHTML = 'Nível Moleza';});
-document.getElementById('atualizaTabuleiroOculto2').addEventListener('click', () => {inicioDeJogo(30), document.getElementById('nivelDoJogo').innerHTML = 'Nível Fácil';});
-document.getElementById('atualizaTabuleiroOculto3').addEventListener('click', () => {inicioDeJogo(40), document.getElementById('nivelDoJogo').innerHTML = 'Nível Médio';});
-document.getElementById('atualizaTabuleiroOculto4').addEventListener('click', () => {inicioDeJogo(50), document.getElementById('nivelDoJogo').innerHTML = 'Nível Difícil';});
-document.getElementById('atualizaTabuleiroOculto5').addEventListener('click', () => {inicioDeJogo(65), document.getElementById('nivelDoJogo').innerHTML = 'Nível Extremo';});
 
 
 function inicioDeJogo(quantidade) {
-    
-    let tabuleiroAtual = [[3, 8, 4, 1, 5, 7, 2, 9, 6], [5, 2, 9, 3, 4, 6, 1, 7, 8], [7, 6, 1, 9, 8, 2, 4, 3, 5], [4, 9, 6, 7, 2, 8, 5, 1, 3], [1, 3, 8, 6, 9, 5, 7, 2, 4], [2, 5, 7, 4, 1, 3, 8, 6, 9], [9, 7, 5, 8, 6, 1, 3, 4, 2], [8, 4, 3, 2, 7, 9, 6, 5, 1], [6, 1, 2, 5, 3, 4, 9, 8, 7]]; 
+    tabuleiroAtual = [[3, 8, 4, 1, 5, 7, 2, 9, 6], [5, 2, 9, 3, 4, 6, 1, 7, 8], [7, 6, 1, 9, 8, 2, 4, 3, 5], [4, 9, 6, 7, 2, 8, 5, 1, 3], [1, 3, 8, 6, 9, 5, 7, 2, 4], [2, 5, 7, 4, 1, 3, 8, 6, 9], [9, 7, 5, 8, 6, 1, 3, 4, 2], [8, 4, 3, 2, 7, 9, 6, 5, 1], [6, 1, 2, 5, 3, 4, 9, 8, 7]]; 
     let posicoesOcultas = geraListaRandomica(quantidade);
     preencheCampoQuantidadeOculta(tabuleiroAtual, posicoesOcultas);
-    verificadorDuranteJogo(tabuleiroAtual,1);
-    verificadorDuranteJogo(tabuleiroAtual,2);
-    verificadorDuranteJogo(tabuleiroAtual,3);
-    verificadorDuranteJogo(tabuleiroAtual,4);
-    verificadorDuranteJogo(tabuleiroAtual,5);
-    verificadorDuranteJogo(tabuleiroAtual,6);
-    verificadorDuranteJogo(tabuleiroAtual,7);
-    verificadorDuranteJogo(tabuleiroAtual,8);
-    verificadorDuranteJogo(tabuleiroAtual,9);
-    verificadorDuranteJogo(tabuleiroAtual,10);
-    verificadorDuranteJogo(tabuleiroAtual,11);
-    verificadorDuranteJogo(tabuleiroAtual,12);
-    verificadorDuranteJogo(tabuleiroAtual,13);
-    verificadorDuranteJogo(tabuleiroAtual,14);
-    verificadorDuranteJogo(tabuleiroAtual,15);
-    verificadorDuranteJogo(tabuleiroAtual,16);
-    verificadorDuranteJogo(tabuleiroAtual,17);
-    verificadorDuranteJogo(tabuleiroAtual,18);
-    verificadorDuranteJogo(tabuleiroAtual,19);
-    verificadorDuranteJogo(tabuleiroAtual,20);
-    verificadorDuranteJogo(tabuleiroAtual,21);
-    verificadorDuranteJogo(tabuleiroAtual,22);
-    verificadorDuranteJogo(tabuleiroAtual,23);
-    verificadorDuranteJogo(tabuleiroAtual,24);
-    verificadorDuranteJogo(tabuleiroAtual,25);
-    verificadorDuranteJogo(tabuleiroAtual,26);
-    verificadorDuranteJogo(tabuleiroAtual,27);
-    verificadorDuranteJogo(tabuleiroAtual,28);
-    verificadorDuranteJogo(tabuleiroAtual,29);
-    verificadorDuranteJogo(tabuleiroAtual,30);
-    verificadorDuranteJogo(tabuleiroAtual,31);
-    verificadorDuranteJogo(tabuleiroAtual,32);
-    verificadorDuranteJogo(tabuleiroAtual,33);
-    verificadorDuranteJogo(tabuleiroAtual,34);
-    verificadorDuranteJogo(tabuleiroAtual,35);
-    verificadorDuranteJogo(tabuleiroAtual,36);
-    verificadorDuranteJogo(tabuleiroAtual,37);
-    verificadorDuranteJogo(tabuleiroAtual,38);
-    verificadorDuranteJogo(tabuleiroAtual,39);
-    verificadorDuranteJogo(tabuleiroAtual,40);
-    verificadorDuranteJogo(tabuleiroAtual,41);
-    verificadorDuranteJogo(tabuleiroAtual,42);
-    verificadorDuranteJogo(tabuleiroAtual,43);
-    verificadorDuranteJogo(tabuleiroAtual,44);
-    verificadorDuranteJogo(tabuleiroAtual,45);
-    verificadorDuranteJogo(tabuleiroAtual,46);
-    verificadorDuranteJogo(tabuleiroAtual,47);
-    verificadorDuranteJogo(tabuleiroAtual,48);
-    verificadorDuranteJogo(tabuleiroAtual,49);
-    verificadorDuranteJogo(tabuleiroAtual,50);
-    verificadorDuranteJogo(tabuleiroAtual,51);
-    verificadorDuranteJogo(tabuleiroAtual,52);
-    verificadorDuranteJogo(tabuleiroAtual,53);
-    verificadorDuranteJogo(tabuleiroAtual,54);
-    verificadorDuranteJogo(tabuleiroAtual,55);
-    verificadorDuranteJogo(tabuleiroAtual,56);
-    verificadorDuranteJogo(tabuleiroAtual,57);
-    verificadorDuranteJogo(tabuleiroAtual,58);
-    verificadorDuranteJogo(tabuleiroAtual,59);
-    verificadorDuranteJogo(tabuleiroAtual,60);
-    verificadorDuranteJogo(tabuleiroAtual,61);
-    verificadorDuranteJogo(tabuleiroAtual,62);
-    verificadorDuranteJogo(tabuleiroAtual,63);
-    verificadorDuranteJogo(tabuleiroAtual,64);
-    verificadorDuranteJogo(tabuleiroAtual,65);
-    verificadorDuranteJogo(tabuleiroAtual,66);
-    verificadorDuranteJogo(tabuleiroAtual,67);
-    verificadorDuranteJogo(tabuleiroAtual,68);
-    verificadorDuranteJogo(tabuleiroAtual,69);
-    verificadorDuranteJogo(tabuleiroAtual,70);
-    verificadorDuranteJogo(tabuleiroAtual,71);
-    verificadorDuranteJogo(tabuleiroAtual,72);
-    verificadorDuranteJogo(tabuleiroAtual,73);
-    verificadorDuranteJogo(tabuleiroAtual,74);
-    verificadorDuranteJogo(tabuleiroAtual,75);
-    verificadorDuranteJogo(tabuleiroAtual,76);
-    verificadorDuranteJogo(tabuleiroAtual,77);
-    verificadorDuranteJogo(tabuleiroAtual,78);
-    verificadorDuranteJogo(tabuleiroAtual,79);
-    verificadorDuranteJogo(tabuleiroAtual,80);
-    verificadorDuranteJogo(tabuleiroAtual,81);
+    document.getElementById('nivelDoJogo').remove();
+    startTimer();
 }
+
+function preencheInputSelecionada(valor) {
+        if (inputSelecionada != null) {
+            inputSelecionada.value = valor;
+            verificaCorreto(tabuleiroAtual);
+        } else {
+            alert("Selecione um campo de input");
+        }
+    }
+
+
 
 function verificaFimDeJogo(tabuleiroAtual) {
     let ganhou = true;
     for (let i = 1; i <= 81; i++) {
         if (document.getElementById(i).value != tabuleiroAtual[i]) {
             ganhou = false;
-        } else {};
+        } else {}
     }
     if (ganhou) {
         exibeVitoria();
     }
 }
 
-
-function verificadorDuranteJogo(tabuleiroAtual, elemento) {
+function verificaCorreto(tabuleiroAtual) {
     let listaTabuleiro = converteTabuleiroemLista(tabuleiroAtual);
-    document.getElementById(elemento).addEventListener('input', function() {
-        const inputValue = document.getElementById(elemento).value;
-        console.log(inputValue);
-        if (inputValue != listaTabuleiro[elemento]) {
-            document.getElementById(elemento).classList.add('error');
+    for (let i = 1; i <= 81; i++) {
+        if (document.getElementById(i).value == ' ') {
+        } else if (document.getElementById(i).value != listaTabuleiro[i]) {
+            document.getElementById(i).classList.add('error');
             erros++;
             document.getElementById('mostraErros').innerHTML = 'Número de erros:' + erros;
         } else {
-            document.getElementById(elemento).classList.remove('error');
+            document.getElementById(i).classList.remove('error');
             verificaFimDeJogo(listaTabuleiro);
         }
-    });
+    }
 }
+
 
 function converteTabuleiroemLista(tabuleiro) {
     let novaLista = [0];
@@ -150,7 +99,6 @@ function converteTabuleiroemLista(tabuleiro) {
     }
     return novaLista;
 }
-
 
 function preencheCampoQuantidadeOculta(tabuleiro, ocultos){
     removeNiveis();
@@ -182,7 +130,6 @@ function geraListaRandomica(quantidade) {
     return randomList;
 }
 
-
 function removeNiveis() {
     document.getElementById('atualizaTabuleiroOculto1').remove();
     document.getElementById('atualizaTabuleiroOculto2').remove();
@@ -193,11 +140,54 @@ function removeNiveis() {
 }
 
 function exibeVitoria() {
-    console.log('ganhou');
-    document.getElementById('nivelDoJogo').innerHTML = 'Você ganhou!';
-    document.getElementById('tabuleiroGrande').remove();
-    document.getElementById('botoesNum').remove();
-    document.getElementById('loader').innerHTML = '<br><br>Muito bem!<br>Vamos adicionar seu nome no Ranking:<br><br><br><input type="text"></div><br><br><br><input type="button" value="Enviar" class="atualiza"><br><br><br>';
-    
+    stopTimer();
+    document.getElementById('tituloPagina').innerHTML = 'Você ganhou!';
+    document.getElementById('botoesNum').style.display = 'none';
+    document.getElementById('loader').innerHTML = '<br><br>Muito bem!<br>Vamos adicionar seu nome no Ranking:<br><br>'
+    document.getElementById('jogarNovamente').style.display = '';
+    document.getElementById('registraRanking').style.display = '';
+    document.getElementById('digitaNome').style.display = '';
 }
 
+let timerInterval;
+let elapsedTime = 0; // Tempo em segundos
+
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+}
+
+function updateTimer() {
+    elapsedTime++;
+    document.getElementById('timer').textContent = formatTime(elapsedTime);
+}
+
+function startTimer() {
+    if (!timerInterval) {
+        timerInterval = setInterval(updateTimer, 1000); // Atualiza o timer a cada segundo
+    }
+}
+
+function stopTimer() {
+    clearInterval(timerInterval);
+    timerInterval = null;
+}
+
+function resetTimer() {
+    elapsedTime = 0;
+    document.getElementById('timer').textContent = formatTime(elapsedTime);
+}
+
+
+function registraRanking() {
+    let nome = document.getElementById('digitaNome').value;
+    let tempo = elapsedTime;
+    let numeroDeErros = erros;
+    document.getElementById('tituloPagina').innerHTML = nome + "<br>Você terminou em " + formatTime(tempo) + " minutos.<br>Número de erros: " + numeroDeErros;
+    document.getElementById('jogarNovamente').style.display = 'none';
+    document.getElementById('registraRanking').style.display = 'none';
+    document.getElementById('digitaNome').style.display = 'none';
+    document.getElementById('timer').style.display = 'none';
+    document.getElementById('loader').style.display = 'none';
+}
